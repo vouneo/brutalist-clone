27b202909b204a6cc8b38d99bb1e969876b04579
vouneo <vouneo.dev@gmail.com>
2024-03-20 23:00:33 -0300

    agregando iconos

--- src/lib/components/AsideMenu.svelte
+++ src/lib/components/AsideMenu.svelte
@@ -1,7 +1,16 @@
 <script>
-    import LibraryIcon from '$lib/icons/library.svg'
+    import LibraryIcon from '$lib/icons/Library.svelte'
+    import HomeIcon from '$lib/icons/Home.svelte'
+    import SearchIcon from '$lib/icons/Search.svelte'
 </script>
 
 <nav class="flex flex-col flex-1 gap-2">
+    <div class="rounded-lg p-2">
+        <ul>
 
+        </ul>
+    </div>
+    <HomeIcon />
+    <SearchIcon />
+    <LibraryIcon />
 </nav>
